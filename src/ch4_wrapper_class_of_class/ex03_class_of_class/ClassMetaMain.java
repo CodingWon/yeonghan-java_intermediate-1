60afb49ff487e5db67ec6f6999e8abc4df43f9bd
kjw <kjw2298@gmail.com>
2024-07-22 16:12:03 +0900

    ch4_wrapper_class_of_class _ ex03_class_of_class
    1) 타입 정보 얻기 : 클래스의 이름, 슈퍼클래스, 인터페이스, 접근 제한자 등과 같은 정보를 조회할 수 있다.
    2) 리플렉션 : 클래스에 정의된 메서드, 필드, 생성자 등을 조회하고, 이들을 통해 객체 인스턴스를 생성하거나 메서드
    를 호출하는 등의 작업을 할 수 있다.
    3) 동적 로딩과 생성 :  `Class.forName()` 메서드를 사용하여 클래스를 동적으로 로드하고, `newInstance()`
    메서드를 통해 새로운 인스턴스를 생성할 수 있다.
    4) 애노테이션 처리 : 클래스에 적용된 애노테이션(annotation)을 조회하고 처리하는 기능을 제공한다

---
+++ src/ch4_wrapper_class_of_class/ex03_class_of_class/ClassMetaMain.java
@@ -0,0 +1,44 @@
+package ch4_wrapper_class_of_class.ex03_class_of_class;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.Struct;
+
+public class ClassMetaMain {
+    public static void main(String[] args) throws Exception {
+        //Class 조회
+        Class clazz1 = String.class;     // 1.클래스에서 조회
+        Class clazz2 = new String().getClass(); // 2.인스턴스에서 조회
+        Class clazz3 = Class.forName("java.lang.String"); // 3.문자열로 조회
+
+        System.out.println("clazz1 : " + clazz1);
+        System.out.println("clazz2 : " + clazz2);
+        System.out.println("clazz3 : " + clazz3);
+
+        // 모든 필드 출력
+        System.out.println("모든 필드 출력 ======");
+        Field[] fields = clazz1.getDeclaredFields();
+        for(Field field : fields){
+            System.out.println("Field : " + field.getType() + " " + field.getName());
+        }
+
+        // 모든 메서드 출력
+        System.out.println("모든 메서드 출력 ======");
+        Method[] methods = clazz1.getDeclaredMethods();
+        for(Method method : methods){
+            System.out.println("Method : " + method);
+        }
+
+        //상위 클래스 정보 출력
+        System.out.println("상위 클래스 정보 출력 ======");
+        System.out.println("Superclass : " + clazz1.getSuperclass().getName());
+
+
+        // 인터페이스 정보 출력
+        System.out.println("인터페이스 정보 출력 ======");
+        Class[] interfaces = clazz1.getInterfaces();
+        for (Class i : interfaces){
+            System.out.println("Interface : " + i.getName());
+        }
+    }
+}
