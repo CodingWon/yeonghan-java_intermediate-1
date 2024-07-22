98ed77a2bb2200ca8b61147fdf02da5bee365ee6
kjw <kjw2298@gmail.com>
2024-07-22 15:09:20 +0900

    ch3_string_class - pro4 5 scount IndexOf 활용

---
+++ src/ch3_string_class/pro5_count/TestString5.java
@@ -0,0 +1,19 @@
+package ch3_string_class.pro5_count;
+
+public class TestString5 {
+    public static void main(String[] args) {
+        String str = "start hello java, hello spring, hello jpa";
+        String key = "hello";
+
+        int count = 0;
+        int index = str.indexOf(key);
+
+        while (index >= 0){
+            System.out.println("index : " + index);
+            index = str.indexOf(key, index +1);
+            count++;
+        }
+        System.out.println("count = " + count);
+
+    }
+}
