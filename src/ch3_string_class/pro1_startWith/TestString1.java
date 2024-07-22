e2fc28f03a3f57b8469181ba1d5da952bf083bc0
kjw <kjw2298@gmail.com>
2024-07-22 14:40:11 +0900

    ch3_string_class - pro1_startWith ã활ì

---
+++ src/ch3_string_class/pro1_startWith/TestString1.java
@@ -0,0 +1,9 @@
+package ch3_string_class.pro1_startWith;
+
+public class TestString1 {
+    public static void main(String[] args) {
+        String url = "https://www.example.com";
+        boolean result = url.startsWith("https://");
+        System.out.println(result);
+    }
+}
