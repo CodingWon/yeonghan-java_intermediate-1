d515b84a71d7f4f2d67e0ba920448c5c893d0b17
kjw <kjw2298@gmail.com>
2024-07-23 13:25:42 +0900

    ch5_enum -  ex06_type_safety_enum_type_refactorying_after

--- src/ch5_enum/ex06_enum_refactoring/DiscountService.java
+++ src/ch5_enum/ex06_enum_refactoring/DiscountService.java
@@ -3,17 +3,6 @@ package ch5_enum.ex06_enum_refactoring;
 public class DiscountService {
 
     public int discount(Grade grade, int price){
-        int discountPercent = 0;
-
-        if(grade == Grade.BASIC){
-            discountPercent = 10;
-        }else if(grade == Grade.GOLD){
-            discountPercent = 20;
-        } else if (grade == Grade.DIAMOND) {
-            discountPercent = 30;
-        }else {
-            System.out.println("할인 x");
-        }
-        return price *  discountPercent /100;
+        return price * grade.getDiscountPercent() / 100;
     }
 }
--- src/ch5_enum/ex06_enum_refactoring/Grade.java
+++ src/ch5_enum/ex06_enum_refactoring/Grade.java
@@ -1,5 +1,14 @@
 package ch5_enum.ex06_enum_refactoring;
 
 public enum Grade {
-    BASIC, GOLD, DIAMOND
+    BASIC(10), GOLD(20), DIAMOND(30);
+    private final int discountPercent;
+
+    Grade (int discountPercent){
+        this.discountPercent = discountPercent;
+    }
+
+    public int getDiscountPercent(){
+        return discountPercent;
+    }
 }
