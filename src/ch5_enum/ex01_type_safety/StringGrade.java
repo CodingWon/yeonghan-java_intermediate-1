030ba06814e73c11584300d020515874ffc57505
kjw <kjw2298@gmail.com>
2024-07-23 11:11:29 +0900

    ch5_enum - ex01_type_safety_1
    - 클래스를 만들어서 상수를 정의

---
+++ src/ch5_enum/ex01_type_safety/StringGrade.java
@@ -0,0 +1,7 @@
+package ch5_enum.ex01_type_safety;
+
+public class StringGrade {
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+}
