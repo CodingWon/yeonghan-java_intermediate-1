aa805b12cdd8728668dac9b2d52f76e3fd9f6902
kjw <kjw2298@gmail.com>
2024-07-23 13:13:13 +0900

    ch5_enum -  ex05_type_safety_enum_type_refactoring_after

--- src/ch5_enum/ex05_enum_refactoring/ClassGrade.java
+++ src/ch5_enum/ex05_enum_refactoring/ClassGrade.java
@@ -1,10 +1,18 @@
 package ch5_enum.ex05_enum_refactoring;
 
 public class ClassGrade {
-    public static final ClassGrade BASIC = new ClassGrade();
-    public static final ClassGrade GOLD = new ClassGrade();
-    public static final ClassGrade DIAMOND = new ClassGrade();
+    public static final ClassGrade BASIC = new ClassGrade(10);
+    public static final ClassGrade GOLD = new ClassGrade(20);
+    public static final ClassGrade DIAMOND = new ClassGrade(30);
+
+    private final int discountPercent;
 
     //타입 열거형 완성 패턴
-    private ClassGrade(){}
+    private ClassGrade(int discountPercent){
+        this.discountPercent = discountPercent;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
 }
--- src/ch5_enum/ex05_enum_refactoring/DiscountService.java
+++ src/ch5_enum/ex05_enum_refactoring/DiscountService.java
@@ -3,17 +3,6 @@ package ch5_enum.ex05_enum_refactoring;
 public class DiscountService {
 
     public int discount(ClassGrade classGrade, int price){
-        int discountPercent = 0;
-
-        if(classGrade == ClassGrade.BASIC){
-            discountPercent = 10;
-        }else if(classGrade == ClassGrade.GOLD){
-            discountPercent = 20;
-        } else if (classGrade == ClassGrade.DIAMOND) {
-            discountPercent = 30;
-        }else {
-            System.out.println("할인 x");
-        }
-        return price *  discountPercent /100;
+        return price * classGrade.getDiscountPercent()/100;
     }
 }
