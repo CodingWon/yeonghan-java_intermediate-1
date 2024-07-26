c11e6e64a7e39090b16f41b724d754f01b461420
kjw <kjw2298@gmail.com>
2024-07-26 09:50:46 +0900

    ch7_inner_nesting_class - ex04_inner_class_활용 - 중첩 클래스 설명

--- src/ch7_inner_nesting_class/ex04_inner_class_활용/Car.java
+++ src/ch7_inner_nesting_class/ex04_inner_class_활용/Car.java
@@ -1,5 +1,14 @@
 package ch7_inner_nesting_class.ex04_inner_class_활용;
 
+/*
+*  중첩 클래스는 언제 사용해야 하나?
+*   - 특정 클래스가 다른 하나의 클래스 안에서만 사용되거나, 둘이 아주 긴밀하게 연결되어 있는 특별한 경우 사용해야 한다.
+* 
+*  중첩 클래스 사용 이유
+*   - 논리적 그룹화
+*   - 캡슐화
+* */
+
 public class Car {
 
     private String model;
