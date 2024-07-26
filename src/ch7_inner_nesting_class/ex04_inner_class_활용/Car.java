beaa73ac2294d2c7504ebb5016dd699363f74dc0
kjw <kjw2298@gmail.com>
2024-07-26 09:47:50 +0900

    ch7_inner_nesting_class - ex04_inner_class_활용 - 리팩토링

--- src/ch7_inner_nesting_class/ex04_inner_class_활용/Car.java
+++ src/ch7_inner_nesting_class/ex04_inner_class_활용/Car.java
@@ -9,19 +9,20 @@ public class Car {
     public Car(String model, int chargeLevel) {
         this.model = model;
         this.chargeLevel = chargeLevel;
-        this.engine = new Engine(this);
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getChargeLevel() {
-        return chargeLevel;
+        this.engine = new Engine();
     }
 
     public void start(){
         engine.start();
         System.out.println(model + " 시작 완료");
     }
+
+    private class Engine {
+        public void start(){
+            System.out.println("충전 레벨 확인 : " + chargeLevel);
+            System.out.println(model + " 의 엔진을 구동합니다.");
+        }
+    }
+
+
 }
