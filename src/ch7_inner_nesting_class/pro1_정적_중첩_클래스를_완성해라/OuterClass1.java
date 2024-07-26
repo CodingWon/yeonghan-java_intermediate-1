915bc95aa25ebc4f0d659b7022b1d80993f52672
kjw <kjw2298@gmail.com>
2024-07-26 16:28:33 +0900

    ch7_inner_nesting_class.pro1_정적_중첩_클래스를_완성해라

---
+++ src/ch7_inner_nesting_class/pro1_정적_중첩_클래스를_완성해라/OuterClass1.java
@@ -0,0 +1,10 @@
+package ch7_inner_nesting_class.pro1_정적_중첩_클래스를_완성해라;
+
+public class OuterClass1 {
+
+    static class NestedClass{
+        public void hello(){
+            System.out.println("NestedClass.hello");
+        }
+    }
+}
---
+++ src/ch7_inner_nesting_class/pro1_정적_중첩_클래스를_완성해라/OuterClassMain.java
@@ -0,0 +1,9 @@
+package ch7_inner_nesting_class.pro1_정적_중첩_클래스를_완성해라;
+
+public class OuterClassMain {
+
+    public static void main(String[] args) {
+        OuterClass1.NestedClass nested = new OuterClass1.NestedClass();
+        nested.hello();
+    }
+}
