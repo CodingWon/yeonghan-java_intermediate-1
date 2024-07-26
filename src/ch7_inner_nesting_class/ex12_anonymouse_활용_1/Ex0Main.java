ed2e8aae8df5f5e8b53fa54d21f4e358e6d109e1
kjw <kjw2298@gmail.com>
2024-07-26 11:05:50 +0900

    ch7_inner_nesting_class - ex12 - 익명 클래스_활용법-2 : 리팩토링후

--- src/ch7_inner_nesting_class/ex12_anonymouse_활용_1/Ex0Main.java
+++ src/ch7_inner_nesting_class/ex12_anonymouse_활용_1/Ex0Main.java
@@ -1,20 +1,14 @@
 package ch7_inner_nesting_class.ex12_anonymouse_활용_1;
 
 public class Ex0Main {
-   public static void helloJava(){
+   public static void print(String str){
        System.out.println("프로그램 시작 ");
-       System.out.println(" Hello Java ");
+       System.out.println(str);
        System.out.println("프로그램 종료 ");
    }
 
-    public static void helloSpring(){
-        System.out.println("프로그램 시작 ");
-        System.out.println(" hello Spring ");
-        System.out.println("프로그램 종료 ");
-    }
-
     public static void main(String[] args) {
-        helloJava();
-        helloSpring();
+       print("helloJava");
+       print("helloSpring");
     }
 }
