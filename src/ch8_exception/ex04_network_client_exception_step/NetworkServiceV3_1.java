57953d605b7b63df1c1ec553e97dd93439dfb6e7
kjw <kjw2298@gmail.com>
2024-07-30 10:50:31 +0900

    ch8_exception.ex04_network_client_예외_메시지 추가

--- src/ch8_exception/ex04_network_client_exception_step/NetworkServiceV3_1.java
+++ src/ch8_exception/ex04_network_client_exception_step/NetworkServiceV3_1.java
@@ -12,9 +12,13 @@ public class NetworkServiceV3_1 {
             client.connect();
             client.send(data);
         }catch (ConnectExceptionV3 e){
-
-        }catch (SendExceptionV3 e){
-
+            System.out.println("[연결 오류] 주소 : " + e.getAddress() + ", 메시지 : " + e.getMessage());
+        } catch (SendExceptionV3 e){
+            System.out.println("[전송 오류] 주소 : " + e.getSendData() + " , 메시지 : " + e.getMessage());
+        }catch (Exception e){
+            System.out.println("[알 수 없는 오류] 메시지 : " + e.getMessage());
+        }finally {
+            client.disconnect();
         }
 
     }
